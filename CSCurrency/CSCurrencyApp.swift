8a0d0d457119441f421c01548e653b2da02be872
Daniel Brezina <danek.brezina@gmail.com>
2025-03-06 09:57:53 +0100

    initial commit

---
+++ CSCurrency/CSCurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CSCurrencyApp.swift
+//  CSCurrency
+//
+//  Created by Daniel Brezina on 06.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct CSCurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
