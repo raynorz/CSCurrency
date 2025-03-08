680986ad2ed55750d15905efc8e43cea17b4e9e6
Daniel Brezina <danek.brezina@gmail.com>
2025-03-08 10:35:58 +0100

    improve existing UI design

--- CSCurrency/Screens/CurrenciesList/CurrenciesListView.swift
+++ CSCurrency/Screens/CurrenciesList/CurrenciesListView.swift
@@ -7,6 +7,76 @@
 
 import SwiftUI
 
+struct CurrencyCircle: View {
+    private let shortName: String
+    private let backgroundColor: Color
+    
+    init(shortName: String, backgroundColor: Color = .blue) {
+        self.shortName = shortName
+        self.backgroundColor = backgroundColor
+    }
+    
+    var body: some View {
+        Text(shortName)
+            .font(.footnote)
+            .frame(width: 32, height: 32)
+            .foregroundColor(.white)
+            .background(
+                Circle()
+                    .fill(backgroundColor.gradient)
+            )
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(Color.gray, lineWidth: 1)
+            )
+    }
+}
+
+struct CurrencyRow: View {
+    var currency: any CurrencyDataProtocol
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 16) {
+                HStack {
+                    HStack(spacing: -10) {
+                        CurrencyCircle(shortName: "CZK")
+                        CurrencyCircle(shortName: currency.shortName, backgroundColor: .orange)
+                    }
+                    
+                    Text("CZK / \(currency.shortName)")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("CZK 1")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Image(systemImageName: .arrowRight)
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    
+                    Label(text: "\(currency.shortName) \(currency.valueFormatted)", systemImageName: SystemImage.arrowUpRightCircle)
+                }
+                .font(.subheadline)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 12)
+                .fill(Color.black.opacity(0.7))
+        )
+    }
+}
+
 struct CurrenciesListView: View {
     @ObservedObject private var viewModel: CurrenciesListViewModel
     
@@ -19,26 +89,7 @@ struct CurrenciesListView: View {
     var body: some View {
         List {
             ForEach(viewModel.currencies, id: \.id) { currency in
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(currency.shortName)
-                            .font(.headline)
-                        Text(currency.name)
-                            .font(.subheadline)
-                        Text(currency.country)
-                            .font(.caption)
-                            .foregroundColor(.secondary)
-                    }
-                    Spacer()
-
-                    VStack(alignment: .trailing) {
-                        Text(String(format: "%.3f", currency.valMid))
-                            .font(.headline)
-                        Text("\(String(format: "%.2f", currency.move))")
-                            .font(.caption)
-                            .foregroundColor(currency.move < 0 ? .red : .green)
-                    }
-                }
+                CurrencyRow(currency: currency)
             }
         }
         .onAppear {
